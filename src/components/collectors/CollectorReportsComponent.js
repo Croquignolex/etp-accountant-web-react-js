f93b097127b9d23fa11b4c897fb26657ea17d942
Croquignolex <gpetitalex10@gmail.com>
2021-08-02 14:02:16 +0100

    Improve extprot formatter

--- src/components/collectors/CollectorReportsComponent.js
+++ src/components/collectors/CollectorReportsComponent.js
@@ -1,5 +1,5 @@
 import PropTypes from "prop-types";
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useMemo, useState} from 'react';
 
 import ExcelFile from "react-data-export/dist/ExcelPlugin/components/ExcelFile";
 import ExcelSheet from "react-data-export/dist/ExcelPlugin/elements/ExcelSheet";
@@ -58,13 +58,20 @@ function CollectorReportsComponent({collector, reports, dispatch, request}) {
                     <ExcelColumn label="DATE" value="creation"/>
                     <ExcelColumn label="TYPE" value="type"/>
                     <ExcelColumn label="NATURE" value="label"/>
-                    <ExcelColumn label="ENTREES" value="in"/>
-                    <ExcelColumn label="SORTIES" value="out"/>
-                    <ExcelColumn label="SOLDES" value="balance"/>
+                    <ExcelColumn label="ACTIFS" value="in"/>
+                    <ExcelColumn label="PASSIFS" value="out"/>
                 </ExcelSheet>
             </ExcelFile>
         )
     }
+
+    const inReportData = useMemo(() => {
+        return reports.reduce((acc, val) => acc + parseInt(val.in, 10), 0);
+    }, [reports]);
+
+    const outReportData = useMemo(() => {
+        return reports.reduce((acc, val) => acc + parseInt(val.out, 10), 0);
+    }, [reports]);
 
     // Render
     return (
@@ -104,10 +111,10 @@ function CollectorReportsComponent({collector, reports, dispatch, request}) {
                                                 )
                                             })}
                                             {reports.length !== 0 ? (
-                                                <tr>
+                                                <tr className="text-bold">
                                                     <td colSpan={3}>Total</td>
-                                                    <td>{reports.reduce((acc, val) => acc + parseInt(val.in, 10), 0)}</td>
-                                                    <td>{reports.reduce((acc, val) => acc + parseInt(val.out, 10), 0)}</td>
+                                                    <td className={`${inReportData === outReportData ? 'text-success' : 'text-danger'}`}>{inReportData}</td>
+                                                    <td className={`${inReportData === outReportData ? 'text-success' : 'text-danger'}`}>{outReportData}</td>
                                                 </tr>
                                             ) : (
                                                 <tr>
