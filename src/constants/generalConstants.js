1b3d68e62ae297c5d14c9442f2edf3341f317186
Croquignolex <gpetitalex10@gmail.com>
2021-08-19 10:13:50 +0100

    Save to change

--- src/constants/generalConstants.js
+++ src/constants/generalConstants.js
@@ -4,4 +4,4 @@ export const APP_NAME = process.env.REACT_APP_APP_NAME;
 // Urls
 export const AUTH_URL = process.env.REACT_APP_AUTH_URL;
 export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const API_SERVER_URL = process.env.REACT_APP_API_SER
+export const API_SERVER_URL = process.env.REACT_APP_API_SERVER_URL;
