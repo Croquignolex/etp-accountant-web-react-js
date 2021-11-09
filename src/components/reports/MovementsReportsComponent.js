c4f673ec9b50d18da0c8f389e7d0f4d9214ea1e2
Croquignolex <gpetitalex10@gmail.com>
2021-11-09 15:17:42 +0100

    Ok for this

--- src/components/reports/MovementsReportsComponent.js
+++ src/components/reports/MovementsReportsComponent.js
@@ -73,7 +73,7 @@ function CollectorMovementsComponent({movements, selectedEndDate, selectedStartD
                                     })}
                                     {movements.length === 0 && (
                                         <tr>
-                                            <td colSpan={6}>
+                                            <td colSpan={8}>
                                                 <div className='alert custom-active text-center'>
                                                     Pas de mouvements de caisse
                                                 </div>
